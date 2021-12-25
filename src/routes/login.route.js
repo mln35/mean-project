f3ef69371b890c2b5158a746a536eebc69e0998a
Magamou Gueye <gueyemagamou@gmail.com>
2021-12-25 19:18:58 +0000

    mail-password verifer added

--- src/routes/login.route.js
+++ src/routes/login.route.js
@@ -2,8 +2,9 @@ const express = require('express');
 const route = express.Router();
 const loginService=require('../services/login.service');
 const registerService=require('../services/register.service');
+
 route.get('/user/login',async (req,res)=>{
-    res.render('pages/login');
+    res.render('pages/login', {error:loginService.log.__NO_ERROR});
 });
 route.post('/user/login', async(req, res)=>{
     // registerService.getAllUsers().then((allUser)=>{
@@ -12,17 +13,20 @@ route.post('/user/login', async(req, res)=>{
     // }).catch((e)=>{
     //     console.log(e);
     // });
-    loginService.loginPost(req.body.email).then((user)=>{
-        try{
-            console.log(user.firstname);
-            res.render('main', {user:user});
+    loginService.loginPost(req).then((user)=>{
+        if(user){
+            console.log(`user:${user.firstname} password:${user.password}`);
+            // res.render('main', {user:user});
+            loginService.log.login=1;
+            res.redirect('/');
         }
-        catch(e){
-            console.log(e);
+        else{
+            console.log('Error');
+            res.render('pages/login', {error: 1});
         }
-        
     }).catch((e)=>{
         console.log(e);
     });
 });
+
 module.exports=route;
--- src/services/login.service.js
+++ src/services/login.service.js
@@ -1,14 +1,16 @@
 const User=require('../model/user.model');
 const nodemail=require('nodemailer');
-loginPost= async(_email)=>{
-    let user=await User.findOne({email:_email});
-        if(user){
-            return user;
-        }
+var log={error:0, login:0};
+loginPost= async(req)=>{
+    let user=await User.findOne({email:req.body.email, password:req.body.password});
+    if(user){
+        return user;
+    }
+    
 }
 validEmail=async(_email)=>{
     var mailformat=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(_email.match){
+    if(_email.match(mailformat)){
         console.log(`${_email} is valid`);
     }
     else
@@ -16,4 +18,4 @@ validEmail=async(_email)=>{
         console.log(`${_email} is valid not`);
     }
 }
-module.exports={loginPost,validEmail};
+module.exports={loginPost,validEmail, log};
