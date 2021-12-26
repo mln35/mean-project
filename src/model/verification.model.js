877323792fc84600c319286016880bfd1f0ee36e
mln35 <mouhametlatyr@gmail.com>
2021-12-26 12:18:13 +0000

    registering loop working

---
+++ src/model/verification.model.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    firstname: {
+        type:String,
+        required: true,
+    },
+    lastname: {
+        type:String,
+        required:true
+    },
+    email: {
+        type:String,
+        required:true,
+        unique:true
+    },
+    phone: {
+        type:String,
+    },
+    password: {
+        type:String,
+        required:true,
+        minlength:8
+    },
+    role:{
+        type:String,
+        required:true,
+        default:'USER'
+    },
+    image:{
+        type:String
+    },
+    verified: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    verificationToken:String
+})
+module.exports= mongoose.model('User',UserSchema)
