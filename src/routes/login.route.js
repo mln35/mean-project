c86147e741e7488b52ed21596838d5fcbaaa1bb1
Magamou Gueye <gueyemagamou@gmail.com>
2021-12-27 13:14:37 +0000

    initial commit in upgrade

--- src/routes/login.route.js
+++ src/routes/login.route.js
@@ -3,7 +3,6 @@ const route = express.Router();
 const loginService=require('../services/login.service');
 const bcrypt=require('bcrypt');
 const jwt=require('jsonwebtoken');
-const registerService=require('../services/register.service');
 const User=require('../model/user.model');
 // route.get('/user/login',async (req,res)=>{
 //     res.render('pages/login', {error:loginService.log.__NO_ERROR});
@@ -15,7 +14,7 @@ route.get('/user/login', async(req, res)=>{
     if(tokenCookie){
         console.log('/user/login--get');
         const token=tokenCookie.split(';').filter(t => t.includes('token'))[0].split("=")[1];
-        // console.log(token);
+        console.log(token);
         const decoded = jwt.verify(token, key);
         const user=await User.findById(decoded.id);
         res.render('pages/dashboard');
@@ -37,7 +36,7 @@ route.post('/user/login', async(req, res)=>{
                    expiresIn:'24h'
                });
             //    console.log(`in loginForm token:${token}`);
-            //    res.cookie('token', token);
+               res.cookie('token', token);
                res.render('pages/dashboard', {
                name:user.firstname,
                email:user.email
@@ -56,47 +55,7 @@ route.post('/user/login', async(req, res)=>{
 });
 
 
-// route.get('user/register', async(req, res)=>{
-//     const tokenCookie=req.headers.cookie;
-//     if(tokenCookie!=undefined){
-//         const token=tokenCookie.split('=')[1];
-//         const decoded=jwt.verify(token, 'secretsecret');
-//         const user=await User.findById(decoded.id);
-//         res.redirect('/');
-//     }
-//     res.render('pages/register');
-// })
-// route.post('/user/login', async(req, res)=>{
-//     // registerService.getAllUsers().then((allUser)=>{
-//     //     console.log(req.body);
-//     //     console.log(allUser);
-//     // }).catch((e)=>{
-//     //     console.log(e);
-//     // });
-//     loginService.loginPost(req).then((user)=>{
-//         if(user){
-//             console.log(`user:${user.firstname} password:${user.password}`);
-//             // res.render('main', {user:user});
-//             loginService.log.login=1;
-//             res.redirect('/');
-//         }
-//         else{
-//             console.log('Error');
-//             res.render('pages/login', {error: 1});
-//         }
-//     }).catch((e)=>{
-//         console.log(e);
-//     });
-// });
-// route.get('/user/logout', async(req, res)=>{
-//     try{
-//         res.render('pages/dashboard');
-//     }
-//     catch(error){
-//         console.log(error);
-//     }
-    
-// });
+
 route.post('/user/logout', (req, res)=>{
     res.clearCookie('token');
     console.log('logout');
